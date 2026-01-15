36f7262635f3d3d6edacc3788c89312e6523d045
coder-lescob <gabriel.lescob@outlook.com>
2026-01-15 18:16:40 +0100

    I created the structure of a token and the prototype of a function to classify tokens from strings

---
+++ src/token.h
@@ -0,0 +1,24 @@
+#ifndef TOKEN_FILE
+#define TOKEN_FILE
+
+// The type of a token
+typedef enum {
+    TOKEN_ILLEGAL,
+    TOKEN_BLANK,
+    TOKEN_EOF
+} TokenType;
+
+// The structure of a token
+typedef struct {
+    TokenType type  ;
+    char      word[];
+} Token;
+
+/*
+* Classify a token, if it doesn't match any category it is marked as ILLEGAL
+* @param   str The string of the token
+* @returns The type of the token
+*/
+TokenType ClassifyToken(char *str);
+
+#endif
